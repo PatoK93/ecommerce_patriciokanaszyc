69375944d5c9a7df543c669812a6bfb95dcc92bc
Patricio Agustín Kanaszyc <pato_.20@hotmail.com>
2022-08-14 18:39:49 -0300

    se sube el desafio itemCollection

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { getProductById } from "../../helpers/asyncMock";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../services/firebase/index";
 
 const ItemDetailContainer = ({ addItem }) => {
   const [product, setProduct] = useState({});
@@ -10,9 +11,12 @@ const ItemDetailContainer = ({ addItem }) => {
   const { productId } = useParams();
 
   useEffect(() => {
-    getProductById(productId)
+    getDoc(doc(db, "products", productId))
       .then((response) => {
-        setProduct(response);
+        const values = response.data();
+
+        const product = { id: response.id, ...values };
+        setProduct(product);
       })
       .catch((error) => {
         console.log(error);
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { getProducts, getProductsByCategory } from "../../helpers/asyncMock";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { db } from "../../services/firebase/index";
 
 const ItemListContainer = ({ greeting }) => {
   const [products, setProducts] = useState([]);
@@ -10,23 +11,32 @@ const ItemListContainer = ({ greeting }) => {
   const { categoryId } = useParams();
 
   useEffect(() => {
-    const asyncFunction = categoryId ? getProductsByCategory : getProducts;
+    setLoading(true)
 
-    asyncFunction(categoryId)
-      .then((response) => {
-        setProducts(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    const collectionRef = !categoryId 
+        ? collection(db, 'products')
+        : query(collection(db, 'products'), where('category', '==', categoryId))
+
+    getDocs(collectionRef).then(response => {
+        const products = response.docs.map(doc => {
+            const values = doc.data()
+            return { id: doc.id, ...values}
+        })
+        setProducts(products)
+    }).catch(error => {
+        console.log(error)
+    }).finally(() => {
+        setLoading(false)
+    })
   }, [categoryId]);
 
   if (loading) {
     return <h2>Cargando productos...</h2>;
   }
+
+  if(products.length === 0) {
+    return categoryId ? <h1>No hay productos en nuestra categoria {categoryId}</h1> : <h1>No hay productos disponibles</h1>
+}
 
   return (
     <>
